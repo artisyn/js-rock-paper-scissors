8f26bcc7d5b56ba7bbe78cf31f68217682bbacbd
artisyn <Artioms1@gmail.com>
2022-08-31 15:15:24 +0300

    v1.1

--- script/script.js
+++ script/script.js
@@ -25,7 +25,7 @@ main.innerHTML = `
 			you to play a game of rock paper scissors.
 		</p>
 		<p>
-			If you <span class="green-text">win</span> - you will walk
+			If you <span class="green-text">win or draw</span> - you will walk
 			as a free man
 		</p>
 		<p>
@@ -73,17 +73,15 @@ playerLivesMarkup.classList.add('player-life--container', 'display-none');
 playerLivesMarkup.innerHTML = `
 	<h2>Your lives:</h2>
 	<div class="player-life">
-		<div class="life-circle life1">
-			<img src="./assets/skull2.svg" />
-		</div>
-		<div class="life-circle life2">
-			<img src="./assets/rose.svg" />
-		</div>
-		<div class="life-circle life3">
-			<img src="./assets/bird.svg" />
-		</div>
+		<div class="life-circle life1"></div>
+		<div class="life-circle life2"></div>
+		<div class="life-circle life3"></div>
 	</div>				
 `;
+const skullMarkup = document.createElement('img');
+skullMarkup.src = './assets/skull2.svg';
+const roseMarkup = document.createElement('img');
+roseMarkup.src = './assets/heart.svg';
 const suspiciousManMarkup = document.createElement('div');
 suspiciousManMarkup.classList.add('suspicious-man', 'display-none');
 suspiciousManMarkup.innerHTML = `
@@ -127,11 +125,11 @@ const btnRoundMarkup = document.createElement('div');
 btnRoundMarkup.classList.add('btn-round--container', 'display-none');
 btnRoundMarkup.innerHTML = `
 <div class='round-message'>test</div>
-<button class="btn-round">Start Round</button>
+<button class="btn-round pointer-none">Start Round</button>
 `;
 const timerMarkup = document.createElement('div');
 timerMarkup.classList.add('timer-container', 'display-none');
-timerMarkup.innerHTML = `<h1>3</h1>`;
+timerMarkup.innerHTML = `<h1 class ='timer-text'></h1>`;
 const playerNameMarkup = document.createElement('h2');
 playerNameMarkup.classList.add('player', 'display-none');
 playerNameMarkup.innerHTML = `You`;
@@ -163,6 +161,7 @@ const suspiciousCardContainer = document.querySelector(
 	'.suspicious-card--container'
 );
 const timerContainer = document.querySelector('.timer-container');
+const timerText = document.querySelector('.timer-text');
 const playerLivesContainer = document.querySelector('.player-life--container');
 const playerLife1 = document.querySelector('.life1');
 const playerLife2 = document.querySelector('.life2');
@@ -171,11 +170,19 @@ const playerSelectedCardContainer = document.querySelector('.player-selected');
 const roundBtn = document.querySelector('.btn-round');
 const playerCardsContainer = document.querySelector('.player-cards');
 const roundMessage = document.querySelector('.round-message');
+const player = document.querySelector('.player');
 
 ////////// main variables
 
 let timer;
-let selectedCard;
+let selectedCard = 'none';
+let computerCard = 'none';
+let currentRound = 0;
+let score = {
+	round1: '',
+	round2: '',
+	round3: '',
+};
 
 /////funcions
 
@@ -194,6 +201,180 @@ const changeRoundMessage = (message) => {
 const changeSelctedCard = (card) => {
 	playerSelectedCardContainer.innerHTML = '';
 	playerSelectedCardContainer.appendChild(card);
+};
+const changeSuspiciousCard = (card) => {
+	suspiciousCardContainer.innerHTML = '';
+	suspiciousCardContainer.appendChild(card);
+};
+const changeTimerText = (text) => {
+	timerText.innerText = text;
+};
+const showTimerXTimes = (times) => {
+	let x = times;
+	// adding interval to a variable to be able to remove it later
+	timer = setInterval(() => {
+		if (x === 0) {
+			clearInterval(timer);
+			changeTimerText('');
+			return;
+		}
+		console.log(x);
+		timerText.innerText = x;
+		x--;
+	}, 1000);
+};
+
+const generateRandomCard = () => {
+	const cards = ['rock', 'paper', 'scissors'];
+	const card = cards[Math.floor(Math.random() * 3)];
+	computerCard = card;
+};
+const determineRoundWinner = () => {
+	//draw
+	if (selectedCard === computerCard) {
+		console.log('draw');
+		changeTimerText('Its a Draw!');
+		score[`round${currentRound}`] = 'draw';
+		return;
+	}
+	// player selectes rock
+	if (selectedCard === 'rock') {
+		if (computerCard === 'paper') {
+			console.log('computer wins');
+			changeTimerText('You Loose!');
+			score[`round${currentRound}`] = 'computer';
+		}
+		if (computerCard === 'scissors') {
+			console.log('player wins');
+			changeTimerText('You Win!');
+			score[`round${currentRound}`] = 'player';
+		}
+	}
+	// player selects paper
+	if (selectedCard === 'paper') {
+		if (computerCard === 'rock') {
+			console.log('player wins');
+			changeTimerText('You Win!');
+			score[`round${currentRound}`] = 'player';
+		}
+		if (computerCard === 'scissors') {
+			console.log('computer wins');
+			changeTimerText('You Loose!');
+			score[`round${currentRound}`] = 'computer';
+		}
+	}
+	// player selects scissors
+	if (selectedCard === 'scissors') {
+		if (computerCard === 'rock') {
+			console.log('computer wins');
+			changeTimerText('You Loose!');
+			score[`round${currentRound}`] = 'computer';
+		}
+		if (computerCard === 'paper') {
+			console.log('player wins');
+			changeTimerText('You Win!');
+			score[`round${currentRound}`] = 'player';
+		}
+	}
+};
+
+const showSuspiciousCard = () => {
+	if (computerCard === 'paper') {
+		changeSuspiciousCard(cardPaperMarkup.cloneNode(true));
+	}
+	if (computerCard === 'rock') {
+		changeSuspiciousCard(cardRockMarkup.cloneNode(true));
+	}
+	if (computerCard === 'scissors') {
+		changeSuspiciousCard(cardScissorsMarkup.cloneNode(true));
+	}
+};
+
+const updatePlayerLife = () => {
+	const life = document.querySelector(`.life${currentRound}`);
+	if (score[`round${currentRound}`] === 'draw')
+		life.appendChild(roseMarkup.cloneNode(true));
+	if (score[`round${currentRound}`] === 'player')
+		life.appendChild(roseMarkup.cloneNode(true));
+	if (score[`round${currentRound}`] === 'computer')
+		life.appendChild(skullMarkup.cloneNode(true));
+};
+
+const resetRound = () => {
+	checkIfGameOver();
+	//reset
+	changeSuspiciousCard(cardEmptyMarkup.cloneNode(true));
+	changeSelctedCard(cardEmptyMarkup.cloneNode(true));
+	changeTimerText('');
+	changeRoundMessage('Pick A Card');
+	playerCardsContainer.classList.remove('pointer-none');
+};
+const removeElements = () => {};
+const loadWinningScreen = () => {
+	const finalMessage = document.createElement('h1');
+	finalMessage.classList.add('final-message');
+	mainDiv.remove();
+	const lastDiv = document.createElement('div');
+	lastDiv.classList.add('main');
+	lastDiv.classList.add('pointer-none');
+	lastDiv.style.opacity = '0';
+	lastDiv.style.backgroundImage = 'url(../assets/freeMan.jpg)';
+	finalMessage.innerText = 'Congratulations You Walk As A Free Man!';
+	lastDiv.appendChild(finalMessage);
+	document.body.prepend(lastDiv);
+	setTimeout(() => {
+		lastDiv.style.opacity = 1;
+	}, 1000);
+};
+const loadLosingScreen = () => {
+	const finalMessage = document.createElement('h1');
+	finalMessage.classList.add('final-message');
+	mainDiv.innerHtml = '';
+	mainDiv.remove();
+	const lastDiv = document.createElement('div');
+	lastDiv.classList.add('main');
+	lastDiv.classList.add('pointer-none');
+	lastDiv.style.opacity = '0';
+	lastDiv.style.backgroundImage = 'url(../assets/died.jpg)';
+	finalMessage.style.color = 'red';
+	finalMessage.innerText = 'Oops You Died...';
+	lastDiv.appendChild(finalMessage);
+	document.body.prepend(lastDiv);
+	setTimeout(() => {
+		lastDiv.style.opacity = 1;
+	}, 1000);
+};
+const checkIfGameOver = () => {
+	if (currentRound === 3) {
+		let x = 0;
+		for (let i = 1; i < 4; i++) {
+			if (
+				score[`round${i}`] === 'player' ||
+				score[`round${i}`] === 'draw'
+			)
+				x++;
+		}
+
+		if (x >= 2) {
+			console.log('player won');
+			// new screen player won
+			//fade screen
+			mainDiv.style.opacity = 0; // fadeOut animation
+			player.style.opacity = 0;
+			//show screen
+			wait(2, loadWinningScreen);
+		}
+		if (x <= 1) {
+			console.log('computer won');
+			// new screen player lost
+			//fade screen
+			mainDiv.style.opacity = 0; // fadeOut animation
+			player.style.opacity = 0;
+
+			//show screen
+			wait(2, loadLosingScreen);
+		}
+	}
 };
 
 ///main functions
@@ -223,6 +404,24 @@ const stage2Transition = () => {
 	changeRoundMessage('Pick A Card');
 	mainGame();
 };
+const playerVsComputer = () => {
+	generateRandomCard();
+	determineRoundWinner();
+	showSuspiciousCard();
+	updatePlayerLife();
+};
+const playRound = () => {
+	// disable start round button and player cards
+	roundBtn.classList.add('pointer-none');
+	playerCardsContainer.classList.add('pointer-none');
+	// set the rounds played count
+	currentRound = currentRound + 1;
+	changeRoundMessage('');
+	// start timer
+	showTimerXTimes(3);
+	wait(4, playerVsComputer);
+	wait(8, resetRound);
+};
 
 // main game logic
 const mainGame = () => {
@@ -232,25 +431,34 @@ const mainGame = () => {
 		if (e.target.classList.contains('card1')) {
 			changeSelctedCard(cardRockMarkup.cloneNode(true));
 			changeRoundMessage('Press Start');
+			roundBtn.classList.remove('pointer-none');
 			selectedCard = 'rock';
 			console.log(selectedCard);
+			return;
 		}
 		if (e.target.classList.contains('card2')) {
 			changeSelctedCard(cardPaperMarkup.cloneNode(true));
 			changeRoundMessage('Press Start');
+			roundBtn.classList.remove('pointer-none');
 			selectedCard = 'paper';
 			console.log(selectedCard);
+			return;
 		}
 		if (e.target.classList.contains('card3')) {
 			changeSelctedCard(cardScissorsMarkup.cloneNode(true));
 			changeRoundMessage('Press Start');
+			roundBtn.classList.remove('pointer-none');
 			selectedCard = 'scissors';
 			console.log(selectedCard);
+			return;
 		}
-
-		roundBtn.addEventListener('click', (e) => {
-			console.log('start round');
-		});
+	});
+	roundBtn.addEventListener('click', (e) => {
+		// dont start the game if card is not selected
+		if (selectedCard === 'none') return;
+		console.log('start round');
+		playRound();
+		return;
 	});
 };
 // events logic
